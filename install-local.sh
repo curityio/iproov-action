6602c42d0520486795c6ecfb2bc7e0a7d477095b
iggbom <jonas@curity.io>
2023-10-04 10:38:48 -0700

    Fixed path in local install script

--- install-local.sh
+++ install-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Example IDSVR_ROOT=/opt/idsvr
-IDSVR_ROOT=/Users/iggbom/curity/idsvr-8.4.1-iproov/idsvr
+IDSVR_ROOT=/Users/iggbom/curity/idsvr-8.5.1/idsvr
 echo "IDSVR_ROOT is set to ${IDSVR_ROOT}"
 
 PLUGIN_DIR=iproov #The name of the folder to deploy the plugin to
@@ -10,4 +10,4 @@ echo "Using ${PLUGIN_DIR} as plugin directory"
 ./gradlew clean buildPlugin
 
 echo "Copying plugin and dependencies to ${IDSVR_ROOT}/usr/share/plugins/${PLUGIN_DIR}"
-cp -r build/${PLUGIN_DIR} ${IDSVR_ROOT}/usr/share/plugins/
+cp -r build/iProov/ ${IDSVR_ROOT}/usr/share/plugins/${PLUGIN_DIR}
